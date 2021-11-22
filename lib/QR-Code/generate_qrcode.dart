589491e67c31bc2b0027483c364bea0dcad607d5
DMiyalu <dieudonnemiyalu6@gmail.com>
2021-11-22 14:08:17 +0100

    generate qr_code

--- lib/QR-Code/generate_qrcode.dart
+++ lib/QR-Code/generate_qrcode.dart
@@ -1,5 +1,12 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share/share.dart';
 
 class GenerateQRCode extends StatefulWidget {
   const GenerateQRCode({Key? key}) : super(key: key);
@@ -9,14 +16,56 @@ class GenerateQRCode extends StatefulWidget {
 }
 
 class _GenerateQRCodeState extends State<GenerateQRCode> {
+  GlobalKey globalKey = new GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: QrImage(
-        data: "Stanislas Makengo",
-        version: QrVersions.auto,
-        size: 200.0,
-      ),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RepaintBoundary(
+              key: globalKey,
+              child: QrImage(
+                data: "Stanislas Makengo",
+                version: QrVersions.auto,
+                size: 200.0,
+              ),
+            ),
+            SizedBox(height: 30),
+            TextButton(onPressed: _captureAndSharePng, child: Text('Envoyer'))
+          ],
+        ),
+      ),
     );
+  }
+
+  Future<void> _captureAndSharePng() async {
+    print('reading');
+    try {
+      RenderRepaintBoundary boundary =
+          globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+      var image = await boundary.toImage();
+      ByteData? byteData = await image.toByteData(format: ImageByteFormat.png);
+
+      final tempDir = await getTemporaryDirectory();
+      final file = await new File('${tempDir.path}/image.png').create();
+      await file.openWrite;
+
+      // ByteData? byteData = await image.toByteData(format: ImageByteFormat.png);
+      // Uint8List pngBytes = byteData!.buffer.asUint8List();
+
+      // final tempDir = await getTemporaryDirectory();
+      // final file = await new File('${tempDir.path}/image.png').create();
+      // final _imageFile = await file.writeAsBytes(pngBytes);
+      shareFile(file: _imageFile);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> shareFile({required File file}) async {
+    Share.shareFiles([], text: 'Share qr_code generated as png');
   }
 }
