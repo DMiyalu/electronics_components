0e2380a4e76c2f877d0db779dca8c3f81bc8f1b6
DMiyalu <dieudonnemiyalu6@gmail.com>
2021-12-22 16:09:32 +0100

    update nfc process

--- lib/NFC/nfc.dart
+++ lib/NFC/nfc.dart
@@ -1,4 +1,6 @@
+import 'package:electronics_components/home.dart';
 import 'package:flutter/material.dart';
+import 'package:nfc_manager/nfc_manager.dart';
 
 class Nfc extends StatefulWidget {
   const Nfc({Key? key}) : super(key: key);
@@ -8,18 +10,39 @@ class Nfc extends StatefulWidget {
 }
 
 class _NfcState extends State<Nfc> {
+  void startNfcProcess() async {
+    bool isAvailable = await NfcManager.instance.isAvailable();
+
+    // Start Session
+    NfcManager.instance.startSession(
+      onDiscovered: (NfcTag tag) async {
+        print('nfc tag: ${tag.data}');
+
+        Ndef? ndef = Ndef.from(tag);
+        if (ndef == null) {
+          print('Tag is not compatible with NDEF');
+          return;
+        }
+
+        print('ndef value: ${ndef.cachedMessage}');
+      },
+    );
+
+    // Stop Session
+    NfcManager.instance.stopSession();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text('NFC')
-            ]
-          ),
-        )
-      ),
+          child: SingleChildScrollView(
+        child: Column(children: [
+          customButton(context,
+              text: "Start Nfc Process", onPressed: () => startNfcProcess()),
+          Text('NFC')
+        ]),
+      )),
     );
   }
 }
